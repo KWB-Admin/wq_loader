b9aceb5ab6fc5a10af0f9038b3a143de2459b66d
admin <admin@kwb.org>
2025-04-07 13:22:51 -0700

    updated upsert for edited_on

--- wq_etl.py
+++ wq_etl.py
@@ -210,7 +210,7 @@ def build_load_query(data: ndarray, etl_yaml: dict) -> pg.sql.Composed:
     return sql.SQL(
         """
         INSERT INTO {schema_name}.{table} ({col_names}) VALUES ({values})
-        ON CONFLICT {prim_key} DO UPDATE SET {update_col} = Excluded.{update_col}
+        ON CONFLICT {prim_key} DO UPDATE SET {update_col} = Excluded.{update_col}, edited_on = current_timestamp
         """
     ).format(
         schema_name=sql.Identifier(etl_yaml["schema_name"]),
